312fac46408ee424109789784e5f475008ca52ba
Jade Clark-Muth <jade@spaceaccountant.me>
2024-12-11 13:42:04 +0000

    Add right display

--- src/main.c
+++ src/main.c
@@ -5,11 +5,22 @@
 
 #include "ssd1351.h"
 
+#define LEFT_DISPLAY_DIN	5
+#define LEFT_DISPLAY_RST	4
+
+#define RIGHT_DISPLAY_DIN	6
+#define RIGHT_DISPLAY_RST	12
+
 static struct ssd1351* left_display;
+static struct ssd1351* right_display;
 
 static int __init spectre_display_init( void ) {
-	left_display = ssd1351_init( SPI_CHIP0, 5, 4 );
+	left_display = ssd1351_init( SPI_CHIP0, LEFT_DISPLAY_DIN, LEFT_DISPLAY_RST );
 	if ( !left_display ) {
+		return -1;
+	}
+	right_display = ssd1351_init( SPI_CHIP1, RIGHT_DISPLAY_DIN, RIGHT_DISPLAY_RST );
+	if ( !right_display ) {
 		return -1;
 	}
 
@@ -17,7 +28,12 @@ static int __init spectre_display_init( void ) {
 }
 
 static void __exit spectre_display_exit( void ) {
-	ssd1351_exit( left_display );
+	if ( left_display ) {
+		ssd1351_exit( left_display );
+	}
+	if ( right_display ) {
+		ssd1351_exit( right_display );
+	}
 }
 
 MODULE_LICENSE( "GPL" );
